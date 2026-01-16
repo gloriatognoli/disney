2d7bc2367fb04bc662a0d17bfb8db57470461be6
Gloria Tognoli <gloria.tognoli@gmail.com>
2026-01-16 13:41:36 +0100

    componente Footer.jsx + css

--- disney/src/views/components/Footer/Footer.jsx
+++ disney/src/views/components/Footer/Footer.jsx
@@ -1,3 +1,61 @@
 //Footer
 import React from "react";
 import {NavLink} from "react-router-dom";
+import style from "./Footer.module.css";
+
+function Footer(props) {
+    const {courseName, courseLink, navItems} = props;
+
+    const itemList = navItems.map((item) => {
+        // removed activeClassName={style.active} from NavLink props
+        return (
+            <li key={item.url} className="nav-item">
+                <NavLink to={item.url}>
+                    {item.text}
+                </NavLink>
+            </li>
+        )
+    });
+
+    return (
+        <footer className={style.footer}>
+
+            <div className="container-fluid">
+
+                <div className="row">
+
+                    <div className="col">
+                        <nav className={style.footerNav}>
+
+                            <ul className="nav flex-column">
+                                {itemList}
+                            </ul>
+
+                        </nav>
+
+                    </div>
+
+                    <div className="col-md-auto">
+
+                        <div className={`d-flex ${style.copyright}`}>
+
+                            <div id={style.course}>
+                                <a href={courseLink} target="_blank">
+                                    {courseName}
+                                </a>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </footer>
+    )
+
+}
+
+export default Footer;
