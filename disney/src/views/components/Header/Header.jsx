3db62fc71424fdd5140749b9f6654c00ede2200c
Gloria Tognoli <gloria.tognoli@gmail.com>
2026-01-16 13:36:13 +0100

    componente Header.jsx + css

--- disney/src/views/components/Header/Header.jsx
+++ disney/src/views/components/Header/Header.jsx
@@ -8,5 +8,46 @@ import {
     NavItem
 } from 'reactstrap';
 import {NavLink} from "react-router-dom";
+import style from "./Header.module.css";
 
+const Header = (props) => {
+    const {logo, navItems} = props;
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
+
+    const itemList = navItems.map((item) => {
+        // Removed activeClassName={style.active} from RouterLink props
+        return (
+            <NavItem key={item.url} className={style.navItem}>
+                <NavLink to={item.url}
+                         className="nav-link">
+                    {item.text}
+                </NavLink>
+            </NavItem>
+        )
+    });
+
+    return (
+        <div className={style.navBar}>
+            <Navbar expand="md" light>
+                <div className={`container ${style.navbarContainer}`}>
+                    <NavLink to="/">
+                        <img className={style.logo} src={logo} alt=""/>
+                    </NavLink>
+                    <NavbarToggler onClick={toggle}/>
+                    <Collapse isOpen={isOpen} navbar>
+                        <Nav className="mr-auto" navbar>
+                            {itemList}
+                        </Nav>
+                    </Collapse>
+                </div>
+            </Navbar>
+        </div>
+
+    );
+}
+
+export default Header;
 
