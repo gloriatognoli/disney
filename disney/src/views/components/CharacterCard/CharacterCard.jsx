d0a98df82b9e7685bfa1698d2e83fb08df570e97
Gloria Tognoli <gloria.tognoli@gmail.com>
2026-02-10 14:52:50 +0100

    Gestione del caso in cui l'immagine di un personaggio sia undefined

--- disney/src/views/components/CharacterCard/CharacterCard.jsx
+++ disney/src/views/components/CharacterCard/CharacterCard.jsx
@@ -1,13 +1,23 @@
 //CharacterCard
-import React from "react";
 import {Card, CardBody, CardImg, CardTitle} from "reactstrap";
 import style from "./CharacterCard.module.css";
 import {NavLink} from "react-router-dom";
+import logoDisney from "../../../assets/logo_disney.png"
 
-
+//Definizione delle CharacterCard:
+//Gli passiamo le props, che sono i dati dei personaggi che vogliamo renderizzare nella Card (definiti nell'API)
 function CharacterCard(props) {
     const {id, name, image} = props;
 
+    //Gestione del caso in cui non ci sia l'immagine definita per un personaggio:
+    //Definiamo un'immagine di fallback (il nostro logo) e la richiamiamo in caso di Error
+    const fallbackImage = logoDisney;
+    const imageSrc = image || fallbackImage;
+    const handleImageError = (e) => {
+        e.target.src = fallbackImage;
+    };
+
+//Renderizzazione della Card
     return (
         <NavLink to={`/Characters/${id}`}>
             <Card className={style.card}>
@@ -15,8 +25,10 @@ function CharacterCard(props) {
                     loading="lazy"
                     className={style.imageUrl}
                     width="100%"
-                    src={image}
-                    alt={name} />
+                    src={imageSrc}
+                    alt={name}
+                    onError={handleImageError}
+                />
                 <CardBody className="text-center">
                     <CardTitle
                         tag="h5"
